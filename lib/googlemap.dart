e945cbd2badd0edf2332b719e6cf238673640b17
kenntcky <hologrampulsion@gmail.com>
2024-09-10 09:31:21 +0700

    Menambahkan logic back-end ke informasi-informasi gempa.

--- lib/googlemap.dart
+++ lib/googlemap.dart
@@ -1,10 +1,16 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
+import 'dart:math' show cos, sqrt, asin;
 import 'package:belajar_maps/widgets/detail_eq.dart';
 import 'package:belajar_maps/widgets/infosr.dart';
 import 'package:belajar_maps/widgets/tsunami_potential.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:geocoding/geocoding.dart';
+
+const String BMKG_API_URL = 'https://data.bmkg.go.id/DataMKG/TEWS/autogempa.json';
 
 class Googlemapflutter extends StatefulWidget {
   const Googlemapflutter({super.key});
@@ -13,14 +19,61 @@ class Googlemapflutter extends StatefulWidget {
   State<Googlemapflutter> createState() => _GooglemapflutterState();
 }
 
+Future<Position> _determinePosition() async {
+  bool serviceEnabled;
+  LocationPermission permission;
+
+  // Test if location services are enabled.
+  serviceEnabled = await Geolocator.isLocationServiceEnabled();
+  if (!serviceEnabled) {
+    // Location services are not enabled don't continue
+    // accessing the position and request users of the
+    // App to enable the location services.
+    return Future.error('Location services are disabled.');
+  }
+
+  permission = await Geolocator.checkPermission();
+  if (permission == LocationPermission.denied) {
+    permission = await Geolocator.requestPermission();
+    if (permission == LocationPermission.denied) {
+      // Permissions are denied, next time you could try
+      // requesting permissions again (this is also where
+      // Android's shouldShowRequestPermissionRationale
+      // returned true. According to Android guidelines
+      // your App should show an explanatory UI now.
+      return Future.error('Location permissions are denied');
+    }
+  }
+
+  if (permission == LocationPermission.deniedForever) {
+    // Permissions are denied forever, handle appropriately.
+    return Future.error(
+        'Location permissions are permanently denied, we cannot request permissions.');
+  }
+
+  // When we reach here, permissions are granted and we can
+  // continue accessing the position of the device.
+  return await Geolocator.getCurrentPosition();
+}
+
 class _GooglemapflutterState extends State<Googlemapflutter> {
 
-    LatLng posisisekarang = const LatLng(-7.009033662685147, 110.42770385742188);
-    BitmapDescriptor customIcon = BitmapDescriptor.defaultMarker; 
+
+    Position? _currentPosition;
+    BitmapDescriptor customIcon = BitmapDescriptor.defaultMarker;
+    Map<String, dynamic>? gempaData;
+    double? gempaDistance;
+    List<double>? gempaCoordinates;
+    Map<String, double>? coordinates;
+
     @override
     void initState() {
       customMarker();
       super.initState();
+      readData();
+      getGempaCoordinates();
+      getGempaHumanReadableAddress();
+      _getPositionAndCalculateDistance();
     }
 
     void customMarker() {
@@ -35,19 +88,115 @@ class _GooglemapflutterState extends State<Googlemapflutter> {
       );
     }
 
+    Future<void> readData() async {
+      DatabaseReference ref = FirebaseDatabase.instance.ref();
+
+      ref.orderByKey().limitToLast(1).onValue.listen((DatabaseEvent event) async{
+        final data = event.snapshot.value;
+        if (data != null) {
+          // The returned data will be a map where the key is the timestamp
+          // and the value is the earthquake data.
+          Map<String, dynamic> dataMap = Map<String, dynamic>.from(data as Map);
+
+          // Extract the latest earthquake data
+          String latestKey = dataMap.keys.first;
+          Map<String, dynamic> latestEarthquake = Map<String, dynamic>.from(dataMap[latestKey]['Infogempa']['gempa']);
+
+          setState(() {
+            gempaData = latestEarthquake;
+          });
+          getGempaCoordinates();
+        }
+      });
+    }
+
+    void getGempaCoordinates() async {
+      if (gempaData == null) {
+        return;
+      }
+
+      String coordinatesBMKG = gempaData!['Coordinates'];
+
+      // Convert string coordinates dari BMKG API ke array
+      List<String> parts = coordinatesBMKG.split(',');
+      var gempaCoordinatesx = List<double>.filled(2, 0);
+
+      if (parts.length == 2) {
+        gempaCoordinatesx[0] = double.parse(parts[0].trim());
+        gempaCoordinatesx[1] = double.parse(parts[1].trim());
+      }
+
+      setState(() {
+        gempaCoordinates = gempaCoordinatesx;
+      });
+    }
+
+    Future<double> calculateGempaDistance(latitude, longitude) async {
+      if (gempaData == null || _currentPosition == null) return 0.0;
+
+      return calculateDistance(
+        _currentPosition!.latitude,
+        _currentPosition!.longitude,
+        latitude,
+        longitude,
+      );
+    }
+
+    void getGempaHumanReadableAddress() async {
+      if (gempaCoordinates == null) {
+        return;
+      }
+
+      List<Placemark> placemarks = await placemarkFromCoordinates(-4.12,129.79);
+      print(placemarks);
+      Placemark placemark = placemarks[0];
+
+
+}
+
+    void _getPositionAndCalculateDistance() async {
+      try {
+        Position position = await _determinePosition();
+        List<Placemark> placemarks = await placemarkFromCoordinates(gempaCoordinates![0], gempaCoordinates![1]);
+        print(placemarks);
+        setState(() {
+          _currentPosition = position;
+        });
+
+        double distance = await calculateGempaDistance(gempaCoordinates![0], gempaCoordinates![1]);
+        setState(() {
+          gempaDistance = distance;
+        });
+      } catch (e) {
+        print('Error: $e');
+      }
+    }
+
+    double calculateDistance(lat1, lon1, lat2, lon2) {
+      var p = 0.017453292519943295;
+      var c = cos;
+      var a = 0.5 - c((lat2 - lat1) * p)/2 +
+          c(lat1 * p) * c(lat2 * p) *
+              (1 - c((lon2 - lon1) * p))/2;
+      return 12742 * asin(sqrt(a));
+    }
+
   @override
   Widget build(BuildContext context) {
+      // print("Data Gempa: ${gempaData!['Coordinates']}");
     return Scaffold(
-      body: Stack(
+      body: gempaCoordinates == null && gempaData == null && gempaDistance == null
+        ? Center(child: CircularProgressIndicator()) // Show loading if data is not yet available
+        : Stack(
         children: [
           Container(
             child: GoogleMap(initialCameraPosition: CameraPosition(
-              target: posisisekarang,
-              zoom : 10
+              target: LatLng(gempaCoordinates![0], gempaCoordinates![1]),
+              zoom : 2
             ),
             markers: {
               Marker(markerId: MarkerId('1'),
-              position: posisisekarang,
+              position: LatLng(gempaCoordinates![0], gempaCoordinates![1]),
               anchor: const Offset(0.5, 0.5),
               draggable: true,
               icon: customIcon,
@@ -96,7 +245,7 @@ class _GooglemapflutterState extends State<Googlemapflutter> {
                                     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                                     children: [
                                       infosr(
-                                        satuangempa: '2,5',
+                                        satuangempa: gempaData!['Magnitude'],
                                         icongempa: 'assets/icons/sr.png',
                                         namabawah: 'Magnitudo',
                                       ),
@@ -122,7 +271,7 @@ class _GooglemapflutterState extends State<Googlemapflutter> {
                                           ),
                                         ),
                                       infosr(
-                                        satuangempa: '10 KM',
+                                        satuangempa: gempaData!['Kedalaman'],
                                         icongempa: 'assets/icons/map.png',
                                         namabawah: 'Kedalaman',
                                       ),
@@ -159,19 +308,21 @@ class _GooglemapflutterState extends State<Googlemapflutter> {
                                   DetailEq(
                                     headline: "Waktu",
                                     detailicon: "assets/icons/clock.png",
-                                    detaildata: "17 Agustus 2024, 08:10:10 WIB"
+                                    detaildata: "${gempaData!['Tanggal']}, ${gempaData!['Jam']}"
                                     ),
 
                                   DetailEq(
                                     headline: "Koordinat",
                                     detailicon: "assets/icons/coordinate.png",
-                                    detaildata: "91 BT 93 BT 101 LS"
+                                    detaildata: "${gempaData!['Lintang']}, ${gempaData!['Bujur']}"
                                     ),
 
                                   DetailEq(
                                     headline: "Jarak",
                                     detailicon: "assets/icons/distance.png",
-                                    detaildata: "101 KM"
+                                    detaildata: gempaDistance != null
+                                        ? "${gempaDistance!.toStringAsFixed(2)} km"
+                                        : 'Memuat...'
                                     ),
                                 ],
                               ),
@@ -225,6 +376,6 @@ class _GooglemapflutterState extends State<Googlemapflutter> {
           ),
         ],
       ),
-    );
+      );
   }
 }
